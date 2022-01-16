c0005b9087b2612e2729ce0300e4bcca21db1526
Christopher Ponzio <chrisponz11@yahoo.com>
2022-01-15 20:57:06 -0500

    Working JS starter readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Password-Generator
+# Password-Generator# Christopher-Ponzio-s-Portfolio
+
+## Description
+This is a password generator. It will generate a randomized password for the given criteria by the user.
+
+## Installation
+
+
+## Usage
+By Answering a few questions from a windows alert the password will be generated automatically once the Generate Password button is toggled.
+![Password Generator](PasswordGenerator.png)
+
+## Credits
+1. Michelle Blackwell github: https://github/Mblackwellgca
+2. https://stackoverflow.com
+3. https://w3schools.com
+
+## License
+No License
--- script.js
+++ script.js
@@ -14,8 +14,26 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+  var generated = [""];
+  var choose = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var length = window.prompt("How many Characters?(Between 8 to 128)");
 
-  return "xxx";
+  var numbers = window.confirm("Would you like to include numbers?");
+  if (numbers === true) {
+    choose.push("1","2","3","4","5","6","7","8","9");
+  }
+  var special = window.confirm("Would you like to include symbols?");
+  if (special === true) {
+    choose.push("!","@","#","$","*");
+  }
+  var uppercase = window.confirm("Would you like to include uppercase characters?");
+  if (uppercase === true) {
+    choose.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+  }
+  for (var i = 0 ; i < length; i++) {
+    generated += choose[Math.floor(Math.random() *choose.length)];
+  }
+  return generated;
 }
 
 
